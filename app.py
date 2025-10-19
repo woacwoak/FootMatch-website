4c6a3722e76691d0ea6be7d0f1cf5a5094acb3bc
woacwoak <woacwoak@gmail.com>
2025-10-19 16:27:29 +0200

    added available pages and routing for pages

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("log-in.html")
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template("sign-up.html")
+
+@app.route("/available-games")
+def available_games():
+    return render_template("available-games.html")
+
+@app.route("/create-game")
+def create_game():
+    return render_template("create-game.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/sign-up.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const backButton = document.getElementById('backButton');
-
-    if (backButton) {
-        backButton.addEventListener('click', () => {
-            window.location.href = 'index.html';
-        });
-    }
-});
